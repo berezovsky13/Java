dd89d9d8b0df1f057af0d1d9a86a61414e986072
User <User@10.30.0.129>
2019-02-18 20:59:20 +0200

    First save in git

---
+++ TicTakToe/src/tictctoe/Something.java
@@ -0,0 +1,18 @@
+package tictctoe;
+
+public class Something {
+
+	public static void main(String[] args) {
+		String text = "Hi";
+		text = text.toLowerCase();
+		System.out.println(text);
+		int character ='c';//char
+		System.out.println(character);
+		character  = 'A';
+		System.out.println(character - 'A' + 1);
+		
+ 		
+
+	}
+
+}
